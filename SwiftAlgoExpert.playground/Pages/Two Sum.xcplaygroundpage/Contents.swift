a4131a88c684a9bfdbe1ef81175729ef4023f463
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-30 01:44:45 +0300

    Solution 2 added

--- SwiftAlgoExpert.playground/Pages/Two Sum.xcplaygroundpage/Contents.swift
+++ SwiftAlgoExpert.playground/Pages/Two Sum.xcplaygroundpage/Contents.swift
@@ -30,3 +30,21 @@ var numbers: [Int] = [5, 10, -5, -1, 0] // -5 -1 5 10
 
 twoNumberSum(&numbers, 7)
 
+// Solution 2
+
+func twoNumberSumV2(_ array: inout [Int], _ targetSum: Int) -> [Int] {
+    for index in array.indices {
+        let firstNumber = array[index]
+        for secondIndex in array.indices where secondIndex > index {
+            let secondNumber = array[secondIndex]
+            
+            if firstNumber + secondNumber == targetSum {
+                return [firstNumber, secondNumber]
+            }
+        }
+    }
+    
+    return []
+}
+
+twoNumberSumV2(&numbers, 0)
